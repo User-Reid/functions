ac78974d954962e7824531fb937b53208e0d902f
Reid Herrera <reidherrerabusiness@gmail.com>
2025-01-21 11:10:57 -0600

    understanding return values

--- src/main.rs
+++ src/main.rs
@@ -1,20 +1,8 @@
-fn open_store(x: &str) {
-    println!("Opening my pizza store in {x}")
-}
-
-fn bake_pizza(number_of_minutes: i8, type_of_pizza: &str) {
-    println!("Now we are baking a {type_of_pizza} pizza for {number_of_minutes} minutes")
-}
-
-fn swim_in_profit(x: &str) {
-    println!("Making {x} fuckin stacks yo")
+fn square(number: i32) -> i32 {
+    return number * number;
 }
 
 fn main() {
-    open_store("Houston");
-
-    bake_pizza(32, "Hawaiin");
-
-    swim_in_profit("Tacos");
-    swim_in_profit("Banana");
+    let answer: i32 = square(5);
+    println!("The result is {answer}")
 }
