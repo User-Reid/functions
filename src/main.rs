71093f6793b0b24679367aa7ffac70f3c4b4d925
Reid Herrera <reidherrerabusiness@gmail.com>
2025-01-21 09:50:01 -0600

    undestanding variables and paramaters.

--- src/main.rs
+++ src/main.rs
@@ -1,21 +1,20 @@
-fn open_store() {
-    println!("Opening my pizza store")
+fn open_store(x: &str) {
+    println!("Opening my pizza store in {x}")
 }
 
-fn bake_pizza() {
-  println!("Now we are baking the pizza")
+fn bake_pizza(number_of_minutes: i8, type_of_pizza: &str) {
+    println!("Now we are baking a {type_of_pizza} pizza for {number_of_minutes} minutes")
 }
 
-fn swim_in_profit() {
-  println!("Making phat fuckin stacks yo")
+fn swim_in_profit(x: &str) {
+    println!("Making {x} fuckin stacks yo")
 }
 
 fn main() {
-  open_store();
+    open_store("Houston");
 
-  bake_pizza();
+    bake_pizza(32, "Hawaiin");
 
-  swim_in_profit();
-  swim_in_profit();
+    swim_in_profit("Tacos");
+    swim_in_profit("Banana");
 }
-
