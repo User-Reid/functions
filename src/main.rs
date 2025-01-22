eb22f952b36ffa97876b5e53118e941d46016309
Reid Herrera <reidherrerabusiness@gmail.com>
2025-01-21 18:20:31 -0600

    Understanding scopes within scopes and isolated scopes :)

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,10 @@
-fn square(number: i32) -> i32 {
-    number * number
-}
-
 fn main() {
-    let answer: i32 = square(5);
-    println!("The result is {answer}");
-    let answer: i32 = square(10);
-    println!("The result to the second function is {answer}");
+    let multiplier: i32 = 3;
+
+    let calculation = {
+        let value: i32 = 5 + 4;
+        value * multiplier
+    };
+
+    println!("{calculation}")
 }
